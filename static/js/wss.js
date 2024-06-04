52274ddc8ae84316ed9acd5f5137599e6a93640c
xaker <xaker.dnepr@gmail.com>
2024-06-04 07:06:23 +0000

    remove console.logs

--- static/js/wss.js
+++ static/js/wss.js
@@ -4,8 +4,10 @@ let WebSocketConnected = false;
 let callbackWSS;
 
 function sendWSS(action = '', topic = '', payload = '') {
+    console.log('sendWSS ', action, topic, WebSocketConnected, auth);
+
     try {
-        const msg = {action:action, topic:topic, payload:payload, user:user};
+        const msg = { action: action, topic: topic, payload: payload, user: user };
         //console.log('sendWSS', msg, 'WebSocketConnected', WebSocketConnected);
 
         if (WebSocketConnected) {
@@ -15,10 +17,11 @@ function sendWSS(action = '', topic = '', payload = '') {
     } catch (error) {
         //console.log('ERROR func: sendWSS', error);
     }
-    
+
 }
 
-function updateSubscribeWSS(){
+function updateSubscribeWSS() {
+    console.log('updateSubscribeWSS', auth);
     sendWSS('unsubscribeAll');
 
     subscriptions.forEach((topic) => {
@@ -27,18 +30,22 @@ function updateSubscribeWSS(){
 
 }
 
-function clearSubscribeWSS(){
+function clearSubscribeWSS() {
+    console.log('clearSubscribeWSS', auth);
     sendWSS('unsubscribeAll');
     subscriptions = [];
 }
 
-function addSubscribeWSS(topic){
+function addSubscribeWSS(topic) {
+    console.log('addSubscribeWSS', auth, topic);
     subscriptions.push(topic);
     subscriptions = [...new Set(subscriptions)];
     sendWSS('subscribe', topic);
 }
 
 function reconectWebSocket() {
+    console.log('reconectWebSocket', auth);
+
     //console.log('reconectWebSocket');
     disconectWebSocket();
 }
@@ -47,28 +54,30 @@ function disconectWebSocket() {
     try {
         socket.close();
     } catch (error) {
-        
+
     }
 }
 
 function connectWebSocket(from = 'none') {
-    //console.log('start connectWebSocket', from, firstConnection, auth, socket);
+    console.log('start connectWebSocket', from, firstConnection, auth, socket);
 
     try {
         if (auth) {
-            if(WebSocketConnected){
-                disconectWebSocket();
+            if (WebSocketConnected) {
+                // disconectWebSocket();
+                console.log('Websocket already connected, return');
+                return;
             }
 
             //console.log('new WebSocket');
             socket = new WebSocket("wss://" + window.location.host);
 
-            socket.addEventListener('open', function(event) {
-                //console.log("wss - open");
+            socket.addEventListener('open', function (event) {
+                console.log("wss - open");
                 $('#logo').removeClass('noColorImg');
                 WebSocketConnected = true;
 
-                if(!firstConnection){
+                if (!firstConnection) {
                     toastr.clear();
                     //toastr.success("Connected");
                 }
@@ -93,10 +102,10 @@ function connectWebSocket(from = 'none') {
                             toastr.options.timeOut = 10000;
                         }
 
-                        toastr[msgData.type](msgData.text, msgData.title); 
+                        toastr[msgData.type](msgData.text, msgData.title);
                         toastr.options.positionClass = 'toast-top-left';
                         toastr.options.timeOut = 3000;
-                        $('#overlay').fadeOut();   
+                        $('#overlay').fadeOut();
                     } else {
                         callbackWSS(msgData);
                     }
@@ -105,22 +114,24 @@ function connectWebSocket(from = 'none') {
                 }
             });
 
-            socket.addEventListener('close', function(event) {
-                //console.log("wss - close");
+            socket.addEventListener('close', function (event) {
+                console.log("wss - close");
                 $('#logo').addClass('noColorImg');
                 WebSocketConnected = false;
 
                 toastr.clear();
                 //toastr["error"]('Disconnected');
                 // При разрыве связи запускаем повторное подключение через некоторое время
+                console.log('addEventListener close setTimeout connectWebSocket')
                 setTimeout(connectWebSocket, 3000); // Повторное подключение через 3 секунды
             });
 
-        }else{
+        } else {
+            console.log('else setTimeout connectWebSocket')
             setTimeout(connectWebSocket, 3000); // Повторное подключение через 3 секунды
         }
     } catch (error) {
-            
+
     }
 }
 
