edbc4003babd436a541a094add9fa8607398d918
xaker <xaker.dnepr@gmail.com>
2024-08-14 13:00:31 +0000

    remove scale deny

--- static/js/swipe.js
+++ static/js/swipe.js
@@ -1,29 +1,29 @@
-$(document).ready(function () {
+// $(document).ready(function () {
 
-    const container = document.getElementById('container');
-    const hammerGlobal = new Hammer(container, {
-        passive: false
-    });
-    const edgeSwipe = new CustomEvent('edgeSwipe');
+//     const container = document.getElementById('container');
+//     const hammerGlobal = new Hammer(container, {
+//         passive: false
+//     });
+//     const edgeSwipe = new CustomEvent('edgeSwipe');
 
-    let nextEvent = '';
-    hammerGlobal.on('panstart', function (event) {
-        const end = event.center.x;
-        const screenWidth = window.innerWidth;
-        if (end < 50 || end > (screenWidth - 50)) {
-            nextEvent = 'close';
-        }
-        event.srcEvent.preventDefault();
-        event.srcEvent.stopPropagation();
-    });
+//     let nextEvent = '';
+//     hammerGlobal.on('panstart', function (event) {
+//         const end = event.center.x;
+//         const screenWidth = window.innerWidth;
+//         if (end < 50 || end > (screenWidth - 50)) {
+//             nextEvent = 'close';
+//         }
+//         event.srcEvent.preventDefault();
+//         event.srcEvent.stopPropagation();
+//     });
 
-    hammerGlobal.on('panend', function (event) {
-        if (nextEvent === 'close') {
-            window.dispatchEvent(edgeSwipe);
+//     hammerGlobal.on('panend', function (event) {
+//         if (nextEvent === 'close') {
+//             window.dispatchEvent(edgeSwipe);
 
-            event.srcEvent.preventDefault();
-            event.srcEvent.stopPropagation();
-            console.log('event.srcEvent', event.srcEvent);
-        }
-    });
-})
+//             event.srcEvent.preventDefault();
+//             event.srcEvent.stopPropagation();
+//             console.log('event.srcEvent', event.srcEvent);
+//         }
+//     });
+// })
